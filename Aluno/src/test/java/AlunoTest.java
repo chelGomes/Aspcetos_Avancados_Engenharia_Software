0a54032b31fa7cb1f848cfd7a66b55c9f97394e7
chelGomes <68570496+chelGomes@users.noreply.github.com>
2024-01-02 15:09:16 -0300

    projeto inicial da disciplina

---
+++ Aluno/src/test/java/AlunoTest.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlunoTest {
+
+    @Test
+     public void deveImprirDadosAlunoFormando(){
+       String nomeAluno = "jose";
+       String nomeCurso = "Engenharia";
+       Aluno aluno = new Aluno();
+       aluno.setNome(nomeAluno);
+       aluno.setNomeCurso(nomeCurso);
+       assertEquals("Nome do aluno: " + nomeAluno + "nome do curso:" + nomeCurso, aluno.imprimirDiploma());
+    }
+}
